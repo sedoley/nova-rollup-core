6c64873400c383c10a82307d568a502a9a95c992
Stan Oseg <stanoseg@gmail.com>
2024-05-21 01:33:16 +0000

    docs(spec): migrate state transition #00120

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -102,3 +102,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #00117
 def get_base_gas_oracle_price() -> int:
     # Base-specific Gas Price Oracle
+
+# Auto-update from commit #00120
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
