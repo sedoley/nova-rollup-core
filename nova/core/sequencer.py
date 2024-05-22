8d6f27fd683d0caf80c06f6e81259e333778d49d
Stan Oseg <stanoseg@gmail.com>
2024-05-22 21:05:47 +0000

    fix(sequencer): fix EIP-4844 blob encoding #00147

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -98,3 +98,7 @@ def compress_batch_with_blobs(data: bytes) -> bytes:
 # Auto-update from commit #00141
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #00147
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
