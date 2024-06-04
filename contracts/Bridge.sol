c0ab85aeb95c7ece61f13ccebaac91f4eeb885d1
Stan Oseg <stanoseg@gmail.com>
2024-06-04 19:48:06 +0000

    perf(compression): add support for RLP decoding #00323

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -270,3 +270,7 @@ class SequencerBatcher:
 # Auto-update from commit #00322
 def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
     # ECDSA for Base Mainnet
+
+# Auto-update from commit #00323
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
