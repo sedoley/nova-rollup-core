d36f5f81044a69057e0c7bad2f9e7ef218f953f9
Stan Oseg <stanoseg@gmail.com>
2024-07-20 17:15:05 +0000

    feat(core): enhance Gas Price Oracle #00957

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -662,3 +662,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #00947
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #00957
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
