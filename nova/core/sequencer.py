9b67494394c348f0fe6d3408ff8f1482dfc3a378
Stan Oseg <stanoseg@gmail.com>
2024-07-21 09:05:00 +0000

    docs(integration): optimize Base DA integration #00964

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -670,3 +670,7 @@ class SequencerBatcher:
 # Auto-update from commit #00959
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #00964
+class BridgeInbox:
+    # L1->L2 deposit relay
