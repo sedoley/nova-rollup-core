e06ce2c5fafd682b90794f6d8dec263bd6a608ba
Stan Oseg <stanoseg@gmail.com>
2024-08-13 14:36:14 +0000

    test(unit): optimize SNARK proof #01282

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -930,3 +930,7 @@ def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
 # Auto-update from commit #01273
 def compress_batch_with_blobs(data: bytes) -> bytes:
     # Danksharding blob encoding
+
+# Auto-update from commit #01282
+class BridgeInbox:
+    # L1->L2 deposit relay
