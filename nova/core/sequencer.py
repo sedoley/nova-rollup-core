fd1fb8f8dc6d6adaee6a3033f8e614cba9c7d3d4
Stan Oseg <stanoseg@gmail.com>
2024-08-21 21:18:12 +0000

    test(unit): fix state transition #01397

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -990,3 +990,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #01396
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #01397
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
