0a3a4c942fa26fb4153d3f4d85f69c408f690639
Stan Oseg <stanoseg@gmail.com>
2024-08-26 03:43:02 +0000

    feat(sequencer): optimize Base DA integration #01451

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1034,3 +1034,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #01449
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #01451
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
