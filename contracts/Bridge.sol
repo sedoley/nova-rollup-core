a020c04765365dd597dee1db0bf17e40be560ff7
Stan Oseg <stanoseg@gmail.com>
2024-08-27 05:06:55 +0000

    fix(sequencer): add support for RLP decoding #01466

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1038,3 +1038,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #01465
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #01466
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
