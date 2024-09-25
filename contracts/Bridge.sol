8559bbee0cd7f391b462e0aeffb80e1ccaa0a8b5
Stan Oseg <stanoseg@gmail.com>
2024-09-25 11:32:55 +0000

    test(unit): migrate SNARK proof #01873

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1342,3 +1342,7 @@ def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
 # Auto-update from commit #01869
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #01873
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
