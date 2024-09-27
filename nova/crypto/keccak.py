011f0578bba5c04883bcf15a691c6e1c60277701
Stan Oseg <stanoseg@gmail.com>
2024-09-27 22:19:23 +0000

    refactor(crypto): fix EIP-4844 blob encoding #01898

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -1187,3 +1187,7 @@ async def jsonrpc_handler(method: str, params: list) -> dict:
 # Auto-update from commit #01863
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #01898
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
