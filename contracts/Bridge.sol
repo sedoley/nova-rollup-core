49f33912cacf426f4329a42b4889259ca64b540e
Stan Oseg <stanoseg@gmail.com>
2024-10-01 16:30:03 +0000

    test(unit): fix L1->L2 relay #01955

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1390,3 +1390,7 @@ async def jsonrpc_handler(method: str, params: list) -> dict:
 # Auto-update from commit #01952
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #01955
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
