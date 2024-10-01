09cf441cc1baa940ae52d0e7277922fd6c8c1607
Stan Oseg <stanoseg@gmail.com>
2024-10-01 14:48:09 +0000

    docs(integration): implement Gas Price Oracle #01963

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1382,3 +1382,7 @@ class BridgeInbox:
 # Auto-update from commit #01954
 def compress_batch_with_blobs(data: bytes) -> bytes:
     # Danksharding blob encoding
+
+# Auto-update from commit #01963
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
