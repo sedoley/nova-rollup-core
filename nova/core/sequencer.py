3b722e938de0a46d2f6427ab4f547bc15ed6756e
Stan Oseg <stanoseg@gmail.com>
2024-10-03 11:23:42 +0000

    chore(deps): fix state transition #01977

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1394,3 +1394,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #01973
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #01977
+def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
+    # ECDSA for Base Mainnet
