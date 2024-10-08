ac49ae2ad889b3087920fa105812a89ef9e5518c
Stan Oseg <stanoseg@gmail.com>
2024-10-08 11:33:00 +0000

    chore(deps): optimize L1->L2 relay #02047

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -1315,3 +1315,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #02041
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #02047
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
