eeb95727dd1ee0b84b82ea5f42e130bf3ed28afd
Stan Oseg <stanoseg@gmail.com>
2024-10-26 05:00:26 +0000

    test(unit): fix Base DA integration #02295

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1602,3 +1602,7 @@ def generate_snark_proof(input: dict) -> bytes:
 # Auto-update from commit #02275
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #02295
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
