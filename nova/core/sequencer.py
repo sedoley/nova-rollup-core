dd05626274ef20decb874a6662d188bbfa8a8cbb
Stan Oseg <stanoseg@gmail.com>
2024-10-27 16:48:00 +0000

    test(unit): fix state transition #02315

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1634,3 +1634,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #02298
 def compress_batch_with_blobs(data: bytes) -> bytes:
     # Danksharding blob encoding
+
+# Auto-update from commit #02315
+class BridgeInbox:
+    # L1->L2 deposit relay
