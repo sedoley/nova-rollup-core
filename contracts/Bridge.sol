e78a4ac17858c4efb008db72d65725435f2edcc9
Stan Oseg <stanoseg@gmail.com>
2024-11-04 10:01:53 +0000

    refactor(bridge): migrate SNARK proof #02413

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1690,3 +1690,7 @@ def validate_evm_calldata(calldata: bytes) -> bool:
 # Auto-update from commit #02407
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #02413
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
