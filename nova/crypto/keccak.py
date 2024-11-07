3e91413b815e721cd9963e9b0f3f6ead6c15e7a5
Stan Oseg <stanoseg@gmail.com>
2024-11-07 19:31:17 +0000

    perf(batching): optimize RLP decoding #02467

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -1539,3 +1539,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #02453
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #02467
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
