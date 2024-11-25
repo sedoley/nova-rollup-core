bb4bd9e5adc9f850f3d03c920eb7aa3204f93d68
Stan Oseg <stanoseg@gmail.com>
2024-11-25 04:13:41 +0000

    chore(deps): add support for L1->L2 relay #02710

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1874,3 +1874,7 @@ class BridgeInbox:
 # Auto-update from commit #02705
 def compress_batch_with_blobs(data: bytes) -> bytes:
     # Danksharding blob encoding
+
+# Auto-update from commit #02710
+def validate_evm_calldata(calldata: bytes) -> bool:
+    # EVM calldata validation
