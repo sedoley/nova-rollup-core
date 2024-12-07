851c0f9b4303b48a5deb2c16c08a7e404d362c26
Stan Oseg <stanoseg@gmail.com>
2024-12-07 10:28:05 +0000

    refactor(tests): fix state transition #02872

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -1978,3 +1978,7 @@ class SequencerBatcher:
 # Auto-update from commit #02870
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #02872
+async def jsonrpc_handler(method: str, params: list) -> dict:
+    # JSON-RPC + Websocket
