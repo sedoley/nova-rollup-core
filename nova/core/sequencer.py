affb774095686c5ae8040fa6aa8f21a4bc490347
Stan Oseg <stanoseg@gmail.com>
2024-12-09 21:15:46 +0000

    test(unit): migrate Gas Price Oracle #02909

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -2006,3 +2006,7 @@ class BridgeInbox:
 # Auto-update from commit #02907
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #02909
+def validate_evm_calldata(calldata: bytes) -> bool:
+    # EVM calldata validation
