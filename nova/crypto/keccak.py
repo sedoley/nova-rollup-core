a8241e581c6247d5da17585d275efd4b55e9b379
Stan Oseg <stanoseg@gmail.com>
2025-01-20 08:49:23 +0000

    test(unit): fix EIP-4844 blob encoding #03481

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -2299,3 +2299,7 @@ class BridgeInbox:
 # Auto-update from commit #03478
 async def jsonrpc_handler(method: str, params: list) -> dict:
     # JSON-RPC + Websocket
+
+# Auto-update from commit #03481
+class BridgeInbox:
+    # L1->L2 deposit relay
