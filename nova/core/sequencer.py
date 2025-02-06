367c27e7c7b080dd053660a5aa484b1587c14f44
Stan Oseg <stanoseg@gmail.com>
2025-02-06 11:42:31 +0000

    feat(da): add support for state transition #03705

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -2486,3 +2486,7 @@ def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
 # Auto-update from commit #03672
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #03705
+class BridgeInbox:
+    # L1->L2 deposit relay
