57492e9fe6e3c1470bc3d9acaa6f6684a4d99b12
Stan Oseg <stanoseg@gmail.com>
2025-02-12 13:14:38 +0000

    chore(ops): migrate Gas Price Oracle #03793

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2518,3 +2518,7 @@ class SequencerBatcher:
 # Auto-update from commit #03789
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #03793
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
