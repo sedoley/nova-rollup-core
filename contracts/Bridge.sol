37408e8aa3ef02c1fbf5c7ed82edca6ced5bfe46
Stan Oseg <stanoseg@gmail.com>
2025-02-16 02:20:14 +0000

    test(unit): add support for SNARK proof #03846

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2562,3 +2562,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #03842
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #03846
+def validate_evm_calldata(calldata: bytes) -> bool:
+    # EVM calldata validation
