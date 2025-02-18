1f5f79f38f0e4433b93ff140469821c5b6781a18
Stan Oseg <stanoseg@gmail.com>
2025-02-18 14:55:24 +0000

    chore(deps): add support for state transition #03878

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2590,3 +2590,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #03875
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #03878
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
