f5607604cba5e1a73ec9eaf01f7664f05090b290
Stan Oseg <stanoseg@gmail.com>
2025-02-21 06:28:52 +0000

    perf(sequencer): migrate Gas Price Oracle #03906

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -2614,3 +2614,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #03905
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #03906
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
