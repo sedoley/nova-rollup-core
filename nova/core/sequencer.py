4cbba8a31f95b474a1afa487ee40035c5556b72a
Stan Oseg <stanoseg@gmail.com>
2025-03-12 10:04:37 +0000

    perf(sequencer): add support for state transition #04168

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -2830,3 +2830,7 @@ def compress_batch_with_blobs(data: bytes) -> bytes:
 # Auto-update from commit #04165
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #04168
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
