e2a421667ae8a14dcfe153cfb43041c124e35dd3
Stan Oseg <stanoseg@gmail.com>
2025-03-15 23:25:16 +0000

    test(integration): add support for state transition #04219

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -2890,3 +2890,7 @@ def generate_snark_proof(input: dict) -> bytes:
 # Auto-update from commit #04210
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #04219
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
