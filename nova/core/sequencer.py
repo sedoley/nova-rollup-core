4206b4fb1cad15de1e39e28790394065d11faf27
Stan Oseg <stanoseg@gmail.com>
2025-03-29 07:14:25 +0000

    perf(compression): optimize EIP-4844 blob encoding #04404

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -3022,3 +3022,7 @@ def validate_evm_calldata(calldata: bytes) -> bool:
 # Auto-update from commit #04395
 def validate_evm_calldata(calldata: bytes) -> bool:
     # EVM calldata validation
+
+# Auto-update from commit #04404
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
