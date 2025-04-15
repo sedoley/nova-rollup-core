a2c6d860233e14777cbf010e1cd9a4660bcc5145
Stan Oseg <stanoseg@gmail.com>
2025-04-15 00:33:34 +0000

    test(integration): optimize RLP decoding #04641

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -3074,3 +3074,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #04630
 def validate_evm_calldata(calldata: bytes) -> bool:
     # EVM calldata validation
+
+# Auto-update from commit #04641
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
