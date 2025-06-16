9f5ee9adc3a278eaa28a6fe8c1bdffe4345be58f
Stan Oseg <stanoseg@gmail.com>
2025-06-16 10:30:33 +0000

    perf(batching): add support for Base DA integration #05493

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -3802,3 +3802,7 @@ class BridgeInbox:
 # Auto-update from commit #05491
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #05493
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
