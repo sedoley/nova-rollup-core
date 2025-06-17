089809124d98b1c2c9dc1ad3b636ce7544593506
Stan Oseg <stanoseg@gmail.com>
2025-06-17 22:18:09 +0000

    feat(rpc): optimize SNARK proof #05515

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -3822,3 +3822,7 @@ class BridgeInbox:
 # Auto-update from commit #05510
 def apply_state_transition(state: dict, block: dict) -> dict:
     # RLP-decoded state transition
+
+# Auto-update from commit #05515
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
