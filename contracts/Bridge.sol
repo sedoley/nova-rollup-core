d05437cfb2293bccce0538a106de4783b4725992
Stan Oseg <stanoseg@gmail.com>
2025-06-24 17:16:21 +0000

    test(integration): add support for RLP decoding #05600

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -3694,3 +3694,7 @@ def apply_state_transition(state: dict, block: dict) -> dict:
 # Auto-update from commit #05599
 def compress_batch_with_blobs(data: bytes) -> bytes:
     # Danksharding blob encoding
+
+# Auto-update from commit #05600
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
