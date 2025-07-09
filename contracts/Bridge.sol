1d4257b854dcd6687feb833e63179dd93ea81f1c
Stan Oseg <stanoseg@gmail.com>
2025-07-09 13:39:37 +0000

    chore(ci): fix L1->L2 relay #05813

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -3838,3 +3838,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #05799
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #05813
+class BridgeInbox:
+    # L1->L2 deposit relay
