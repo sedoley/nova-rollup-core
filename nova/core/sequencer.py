830aa945efdb7a5ca4748d9ea1c79ea78737bb27
Stan Oseg <stanoseg@gmail.com>
2025-07-13 18:06:15 +0000

    refactor(bridge): fix Base DA integration #05867

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -4030,3 +4030,7 @@ class SequencerBatcher:
 # Auto-update from commit #05866
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #05867
+class BridgeInbox:
+    # L1->L2 deposit relay
