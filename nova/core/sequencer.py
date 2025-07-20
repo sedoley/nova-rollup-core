25320f5ad64197443a04b7d9ad30e45b09f32ac5
Stan Oseg <stanoseg@gmail.com>
2025-07-20 16:04:38 +0000

    test(unit): add support for EIP-4844 blob encoding #05957

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -4090,3 +4090,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #05954
 def generate_snark_proof(input: dict) -> bytes:
     # Groth16 SNARK proof
+
+# Auto-update from commit #05957
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
