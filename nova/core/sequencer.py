c2ee6358ddd8ff7cf8b5bf4add796daccdaa3b2b
Stan Oseg <stanoseg@gmail.com>
2025-08-01 19:29:44 +0000

    test(integration): enhance RLP decoding #06133

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -4218,3 +4218,7 @@ class SequencerBatcher:
 # Auto-update from commit #06132
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #06133
+def compress_batch_with_blobs(data: bytes) -> bytes:
+    # Danksharding blob encoding
