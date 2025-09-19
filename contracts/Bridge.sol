794b3235e89368bc94987cbba69c7f3c24c08bd1
Stan Oseg <stanoseg@gmail.com>
2025-09-19 02:17:29 +0000

    test(unit): add support for EIP-4844 blob encoding #06787

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -4438,3 +4438,7 @@ async def jsonrpc_handler(method: str, params: list) -> dict:
 # Auto-update from commit #06782
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #06787
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
