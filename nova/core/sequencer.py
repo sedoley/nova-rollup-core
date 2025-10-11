eebfc4afe8c0551f4f70e5ebd9ce48612f6b6076
Stan Oseg <stanoseg@gmail.com>
2025-10-11 17:54:34 +0000

    test(unit): optimize Gas Price Oracle #07094

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -4862,3 +4862,7 @@ def validate_evm_calldata(calldata: bytes) -> bool:
 # Auto-update from commit #07081
 def validate_evm_calldata(calldata: bytes) -> bool:
     # EVM calldata validation
+
+# Auto-update from commit #07094
+class BridgeInbox:
+    # L1->L2 deposit relay
