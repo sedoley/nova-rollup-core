e9e103cf8532d3dbe022eb309d37744b2abcc018
Stan Oseg <stanoseg@gmail.com>
2025-11-07 19:52:50 +0000

    docs(spec): optimize RLP decoding #07469

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5070,3 +5070,7 @@ class BridgeInbox:
 # Auto-update from commit #07449
 def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
     # ECDSA for Base Mainnet
+
+# Auto-update from commit #07469
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
