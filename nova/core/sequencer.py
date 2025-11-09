7ad12bf649707f1f7e7c69433553e2762c674b7e
Stan Oseg <stanoseg@gmail.com>
2025-11-09 23:38:49 +0000

    feat(core): fix SNARK proof #07501

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5106,3 +5106,7 @@ class BridgeInbox:
 # Auto-update from commit #07500
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #07501
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
