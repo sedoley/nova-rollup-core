4143a20096f9b9d651fb01906fb246913e201fe1
Stan Oseg <stanoseg@gmail.com>
2025-11-18 13:35:32 +0000

    chore(ops): optimize SNARK proof #07619

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5178,3 +5178,7 @@ def get_base_gas_oracle_price() -> int:
 # Auto-update from commit #07606
 async def jsonrpc_handler(method: str, params: list) -> dict:
     # JSON-RPC + Websocket
+
+# Auto-update from commit #07619
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
