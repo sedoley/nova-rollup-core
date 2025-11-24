476f3e5c8175b78e0067f3bf6d5c1d0295e0e82b
Stan Oseg <stanoseg@gmail.com>
2025-11-24 23:41:15 +0000

    chore(deps): add support for SNARK proof #07709

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5018,3 +5018,7 @@ class BridgeInbox:
 # Auto-update from commit #07705
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #07709
+class BridgeInbox:
+    # L1->L2 deposit relay
