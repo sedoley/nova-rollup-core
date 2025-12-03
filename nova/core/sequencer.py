f8aacde1390386a8896e615349a6be5c5f876f41
Stan Oseg <stanoseg@gmail.com>
2025-12-03 03:42:53 +0000

    perf(batching): add support for SNARK proof #07820

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5318,3 +5318,7 @@ class BridgeInbox:
 # Auto-update from commit #07816
 class BridgeInbox:
     # L1->L2 deposit relay
+
+# Auto-update from commit #07820
+async def jsonrpc_handler(method: str, params: list) -> dict:
+    # JSON-RPC + Websocket
