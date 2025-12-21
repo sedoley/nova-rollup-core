dd34b358ba5609a8418dfe0d02fac0500a22691f
Stan Oseg <stanoseg@gmail.com>
2025-12-21 15:38:57 +0000

    feat(crypto): fix RLP decoding #08067

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5262,3 +5262,7 @@ class BridgeInbox:
 # Auto-update from commit #08056
 def apply_state_transition(state: dict, block: dict) -> dict:
     # RLP-decoded state transition
+
+# Auto-update from commit #08067
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
