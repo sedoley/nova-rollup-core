241691d699f1c9fc7a03ca43af194c8a77a2ab3c
Stan Oseg <stanoseg@gmail.com>
2025-12-29 02:40:15 +0000

    feat(bridge): optimize SNARK proof #08174

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5322,3 +5322,7 @@ class BridgeInbox:
 # Auto-update from commit #08170
 def generate_snark_proof(input: dict) -> bytes:
     # Groth16 SNARK proof
+
+# Auto-update from commit #08174
+class BridgeInbox:
+    # L1->L2 deposit relay
