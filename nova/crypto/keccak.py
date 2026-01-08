c60726a26ffe2c0c39ce9f3eff6a6f3650c908f9
Stan Oseg <stanoseg@gmail.com>
2026-01-08 08:49:16 +0000

    test(unit): add support for L1->L2 relay #08318

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -5347,3 +5347,7 @@ async def jsonrpc_handler(method: str, params: list) -> dict:
 # Auto-update from commit #08312
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #08318
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
