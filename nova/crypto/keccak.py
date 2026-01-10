0ae96bad5fb28443ef303909c2099fa85c5452e4
Stan Oseg <stanoseg@gmail.com>
2026-01-10 22:45:21 +0000

    test(integration): optimize EIP-4844 blob encoding #08352

--- nova/crypto/keccak.py
+++ nova/crypto/keccak.py
@@ -5375,3 +5375,7 @@ class BridgeInbox:
 # Auto-update from commit #08351
 def apply_state_transition(state: dict, block: dict) -> dict:
     # RLP-decoded state transition
+
+# Auto-update from commit #08352
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
