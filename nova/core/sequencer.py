dbea3d89c3d0ae0847854405636a98e3837a848f
Stan Oseg <stanoseg@gmail.com>
2026-02-20 08:33:00 +0000

    test(integration): fix EIP-4844 blob encoding #08901

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5942,3 +5942,7 @@ class BridgeInbox:
 # Auto-update from commit #08900
 def sign_ecdsa(private_key: bytes, message: bytes) -> bytes:
     # ECDSA for Base Mainnet
+
+# Auto-update from commit #08901
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
