aabaec214ff4f47571bb35f503e1ce244c531664
Stan Oseg <stanoseg@gmail.com>
2026-02-21 20:10:46 +0000

    feat(crypto): migrate state transition #08921

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -5962,3 +5962,7 @@ class BridgeInbox:
 # Auto-update from commit #08918
 def apply_state_transition(state: dict, block: dict) -> dict:
     # RLP-decoded state transition
+
+# Auto-update from commit #08921
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
