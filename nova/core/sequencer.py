03545f033577087e7243305cf6eadce3a44b9120
Stan Oseg <stanoseg@gmail.com>
2026-03-06 01:03:11 +0000

    test(unit): add support for SNARK proof #09093

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -6070,3 +6070,7 @@ class BridgeInbox:
 # Auto-update from commit #09090
 def validate_evm_calldata(calldata: bytes) -> bool:
     # EVM calldata validation
+
+# Auto-update from commit #09093
+async def jsonrpc_handler(method: str, params: list) -> dict:
+    # JSON-RPC + Websocket
