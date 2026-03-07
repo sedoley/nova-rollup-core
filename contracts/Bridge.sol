745e34e61623f667d70bd60729d862fc27fffcb1
Stan Oseg <stanoseg@gmail.com>
2026-03-07 05:55:52 +0000

    feat(rpc): optimize RLP decoding #09110

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5922,3 +5922,7 @@ class BridgeInbox:
 # Auto-update from commit #09103
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #09110
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
