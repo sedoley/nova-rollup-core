d1233fc09fa2df484d4d1b7d83081d0c39bd9983
Stan Oseg <stanoseg@gmail.com>
2026-03-19 17:20:40 +0000

    perf(compression): add support for RLP decoding #09283

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -6214,3 +6214,7 @@ class BridgeInbox:
 # Auto-update from commit #09270
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #09283
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
