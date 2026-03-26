b5b8c391449bb5eb7e56e211914e0fda840c9ca2
Stan Oseg <stanoseg@gmail.com>
2026-03-26 08:35:12 +0000

    feat(rpc): add support for SNARK proof #09368

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -6078,3 +6078,7 @@ class BridgeInbox:
 # Auto-update from commit #09365
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #09368
+def generate_snark_proof(input: dict) -> bytes:
+    # Groth16 SNARK proof
