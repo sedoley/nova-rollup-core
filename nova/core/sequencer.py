4062603fbb7dfbe411bdffcf5cafcbe286b9f5fe
Stan Oseg <stanoseg@gmail.com>
2026-03-27 15:30:45 +0000

    refactor(crypto): migrate SNARK proof #09385

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -6270,3 +6270,7 @@ class BridgeInbox:
 # Auto-update from commit #09373
 def verify_keccak256(data: bytes) -> bytes:
     # EIP-4844 compatible Keccak for Base DA
+
+# Auto-update from commit #09385
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
