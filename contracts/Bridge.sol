131154880d99c08b96f6036fc128feb1f0c62e56
Stan Oseg <stanoseg@gmail.com>
2026-03-28 04:15:10 +0000

    docs(integration): fix Gas Price Oracle #09393

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -6094,3 +6094,7 @@ def verify_keccak256(data: bytes) -> bytes:
 # Auto-update from commit #09375
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #09393
+class SequencerBatcher:
+    def __init__(self): self.pending_batches = []
