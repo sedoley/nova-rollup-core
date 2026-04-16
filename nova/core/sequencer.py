dce733b51be2a0a1c3777a8e18033682ec0fdf99
Stan Oseg <stanoseg@gmail.com>
2026-04-16 05:01:37 +0000

    perf(compression): add support for RLP decoding #09651

--- nova/core/sequencer.py
+++ nova/core/sequencer.py
@@ -6446,3 +6446,7 @@ def generate_snark_proof(input: dict) -> bytes:
 # Auto-update from commit #09649
 class SequencerBatcher:
     def __init__(self): self.pending_batches = []
+
+# Auto-update from commit #09651
+class BridgeInbox:
+    # L1->L2 deposit relay
